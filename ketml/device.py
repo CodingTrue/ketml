9dda8dcbd2cef8c7bdd391e5b1696e005bdd4647
CodingTrue <codingtrue@web.de>
2026-04-10 20:43:57 +0200

    Added cl_queue and cl_context to device class

--- ketml/device.py
+++ ketml/device.py
@@ -19,10 +19,28 @@ class Device:
     def __init__(self, cl_device: cl.Device):
         self.cl_device = cl_device
 
+        self.cl_context: cl.Context = None
+        self.cl_queue: cl.CommandQueue = None
+
         self.device_name = get_cl_device_name(cl_device=cl_device)
 
     def use(self):
         Device._default = self
+
+        self.cl_context = cl.Context(devices=[self.cl_device])
+        self.cl_queue = cl.CommandQueue(context=self.cl_context, device=self.cl_device, properties=cl.command_queue_properties.PROFILING_ENABLE)
+
+    def get_context(self) -> cl.Context:
+        if self.cl_context is None:
+            raise RuntimeError("Context of device is None.")
+
+        return self.cl_context
+
+    def get_queue(self) -> cl.CommandQueue:
+        if self.cl_context is None:
+            raise RuntimeError("CommandQueue of device is None.")
+
+        return self.cl_queue
 
     def get_name(self) -> str:
         return self.device_name
